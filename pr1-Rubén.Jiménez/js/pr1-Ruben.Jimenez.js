a429fd4f5b842b9876be9c2fed7ef3827a72c5df
ruben075 <92880382+ruben075@users.noreply.github.com>
2022-02-18 14:24:23 +0100

    Add files via upload

---
+++ pr1-Rubén.Jiménez/js/pr1-Ruben.Jimenez.js
@@ -0,0 +1,13 @@
+function boton13(){
+    document.getElementById("boton11").innerHTML = "Le has dado al boton1";
+}
+function boton23(){
+    document.getElementById("boton22").innerHTML = "Le has dado al boton2";
+}
+function boton33(){
+    document.getElementById("boton22").style.display="none";
+    document.getElementById("boton11").style.display="none";
+}
+function dale(a){
+    document.body.style.backgroundColor = 'green';
+}
