a44e79f78e13ace9ffd4081326893b3e27f8b47e
ruben075 <92880382+ruben075@users.noreply.github.com>
2022-02-18 14:20:40 +0100

    Add files via upload

---
+++ pr2-Rubén.Jiménez/js/java.js
@@ -0,0 +1,11 @@
+function hola1(){
+    alert("has fet click");
+}
+function apretar(){
+    alert("hola has apretat la p");
+}
+function sumar(x,y){
+    var c=x+y;
+    alert(x+y);
+}
+document.body.style.backgroundColor = 'green';
